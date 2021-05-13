8ada0d28079fa9ebeb6dec8083c8222636ab3af8
redmundnacario <redmund.nacario@gmail.com>
2021-05-13 20:37:14 +0800

    fixes modal component

--- src/components/shared/modal/modal.component.jsx
+++ src/components/shared/modal/modal.component.jsx
@@ -1,15 +1,9 @@
 import React, { useContext} from 'react'
 import {Modal as ModalB} from 'react-bootstrap'
 
-// import UserContext from '../../../context/user/userContext'
 import ModalContext from '../../../context/modal/modalContext'
 
-//
-import JournalForms from '../../layout/forms/journals/journal.form.component'
-import TaskForms from '../../layout/forms/tasks/task.form.component'
 
-import JournalDelete from '../../../containers/journal/journal.delete.container'
-import TaskDelete from '../../../containers/task/task.delete.container'
 import UserDelete from '../../../containers/user/user.delete.container'
 
 const Modal = () => {
@@ -17,12 +11,6 @@ const Modal = () => {
     const modalContext = useContext(ModalContext)
     const {show, modalBody, hideModal} = modalContext
     const modalContentDict = {
-        JournalForms : <JournalForms/>,
-        JournalFormsEdit : <JournalForms/>,
-        DeleteJournal : <JournalDelete/>,
-        TaskForms :  <TaskForms/>,
-        TaskFormsEdit : <TaskForms/>,
-        DeleteTask : <TaskDelete/>,
         DeleteUser : <UserDelete/>
     }
     return (
